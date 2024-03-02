5358be3c123b1629dd8829725c4b8a7869c8351b
Abraham Gebrekidan <abrahamkgebrekidan@gmail.com>
2024-03-02 04:24:52 +0300

    replaced img with Image in header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import styles from "./header.module.css";
+import Image from "next/image";
 
 const Header: React.FC = ({}) => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.left}>
-          <img src="/logo-new.svg" alt="Testvalley" className={styles.logo} />
+          <Image
+            src="/logo-new.svg"
+            alt="Testvalley"
+            className={styles.logo}
+            width={0}
+            height={0}
+          />
           <div className={styles.categoryMenu}>category</div>
           <div className={styles.searchBar}>
-            <img src="/search.svg" alt="" className={styles.searchIcon} />
+            <Image
+              src="/search.svg"
+              alt=""
+              className={styles.searchIcon}
+              width={20}
+              height={20}
+            />
             <input
               type="search"
               placeholder="If you're wondering whether to buy it or not, search for it!"
@@ -19,14 +32,32 @@ const Header: React.FC = ({}) => {
         </div>
         <div className={styles.rightDesktop}>
           <button title="home">
-            <img src="/home-event.svg" alt="" />
+            <Image src="/home-event.svg" alt="" width={28} height={28} />
           </button>
-          <img className={styles.bar} src="/vertical-bar.svg" alt="" />
+          <Image
+            className={styles.bar}
+            src="/vertical-bar.svg"
+            alt=""
+            width={1}
+            height={14}
+          />
           <button>Login / Register</button>
         </div>
         <div className={styles.rightMobile}>
-          <img src="/bell_default.svg" alt="" className={styles.iconMobile} />
-          <img src="/search_new.svg" alt="" className={styles.iconMobile} />
+          <Image
+            src="/bell_default.svg"
+            alt=""
+            className={styles.iconMobile}
+            width={48}
+            height={48}
+          />
+          <Image
+            src="/search_new.svg"
+            alt=""
+            className={styles.iconMobile}
+            width={42}
+            height={44}
+          />
         </div>
       </div>
     </header>
