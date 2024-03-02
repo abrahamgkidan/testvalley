c6cf243a8d0462799e92e5f9ea5f8df7229a82f7
Abraham Gebrekidan <abrahamkgebrekidan@gmail.com>
2024-03-02 04:04:52 +0300

    initial setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dvd6ljcj7w3pj.cloudfront.net",
+        port: "",
+        pathname: "/static/main_banner/**",
+      },
+      {
+        protocol: "https",
+        hostname: "dvd6ljcj7w3pj.cloudfront.net",
+        port: "",
+        pathname: "/static/main_shortcut/**",
+      },
+      {
+        protocol: "https",
+        hostname: "dvd6ljcj7w3pj.cloudfront.net",
+        port: "",
+        pathname: "/media/PUBLICATION/**",
+      },
+      {
+        protocol: "https",
+        hostname: "prod-testvalley.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
